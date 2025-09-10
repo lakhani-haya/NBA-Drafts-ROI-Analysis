0073a8e4d9bea226ad546646ca513a707798e4eb
Haya Lakhani <157073204+lakhani-haya@users.noreply.github.com>
2025-09-09 23:44:04 -0500

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,7 +12,7 @@ def load_and_clean_data(file_path):
         pd.DataFrame: Cleaned DataFrame
     """
     
-    # Re
+    # 
     print("Loading CSV file...")
     df = pd.read_csv(file_path)
     print(f"Original data shape: {df.shape}")
